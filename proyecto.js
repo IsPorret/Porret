c4806951be02b27297d30e49181911478d080821
Porret <agustinberrett@gmail.com>
2023-03-09 11:23:38 -0300

    Agregando función en botones par aoptimizar el código

--- proyecto.js
+++ proyecto.js
@@ -1,51 +1,26 @@
 alert("Link del repositorio:     https://github.com/IsPorret/Porret");
 
-const botonName = document.getElementById("boton-name");
-const botonEmail = document.getElementById("boton-email");
-const botonNumber = document.getElementById("boton-number");
-const botonBirthday = document.getElementById("boton-birthday");
 
 
-botonName.addEventListener("focusin", function() {
-  console.log("Mostrar nombre");
-  document.getElementById("details").innerHTML = "Agustin Berretta"
-  document.getElementById("boton-name").style = "background-image: radial-gradient(#f99 1%, #fb7 30%, #F82 60%)"
-});
-
-botonName.addEventListener("focusout", function() {
-document.getElementById("boton-name").style = "background-image: radial-gradient(#f88 1%, #fa6 30%, #F60 60%)";
-});
-
-
-botonEmail.addEventListener("focusin", function() {
-  console.log("Mostrar email");
-  document.getElementById("details").innerHTML = "agustinberrett@gmail.com"
-  document.getElementById("boton-email").style = "background-image: radial-gradient(#f99 1%, #fb7 30%, #F82 60%)";
-});
-
-botonEmail.addEventListener("focusout", function() {
-  document.getElementById("boton-email").style = "background-image: radial-gradient(#f88 1%, #fa6 30%, #F60 60%)";
+function agregarEventoBoton(botonId, detalle) {
+  const boton = document.getElementById(botonId);
+  boton.addEventListener("focusin", function() {
+    console.log(`Mostrar ${detalle}`);
+    document.getElementById("details").innerHTML = detalle;
+    boton.style = "background-image: radial-gradient(#f99 1%, #fb7 30%, #F82 60%)";
   });
-
-botonNumber.addEventListener("focusin", function() {
-  console.log("Mostrar número");
-  document.getElementById("details").innerHTML = "15 - 5455 1515"
-  document.getElementById("boton-number").style = "background-image: radial-gradient(#f99 1%, #fb7 30%, #F82 60%)"
-});
-
-botonNumber.addEventListener("focusout", function() {
-  document.getElementById("boton-number").style = "background-image: radial-gradient(#f88 1%, #fa6 30%, #F60 60%)";
+  boton.addEventListener("focusout", function() {
+    boton.style = "background-image: radial-gradient(#f88 1%, #fa6 30%, #F60 60%)";
   });
+}
 
-botonBirthday.addEventListener("focusin", function() {
-  console.log("Mostrar cumpleaños");
-  document.getElementById("details").innerHTML = "13/03 (1992)"
-  document.getElementById("boton-birthday").style = "background-image: radial-gradient(#f99 1%, #fb7 30%, #F82 60%)"
-});
+const botones = [
+  { id: "boton-name", detalle: "Agustin Berretta" },
+  { id: "boton-email", detalle: "agustinberrett@gmail.com" },
+  { id: "boton-number", detalle: "15 - 5455 1515" },
+  { id: "boton-birthday", detalle: "13/03 (1992)" },
+];
 
-botonBirthday.addEventListener("focusout", function() {
-  document.getElementById("boton-birthday").style = "background-image: radial-gradient(#f88 1%, #fa6 30%, #F60 60%)";
-  });
-
-
-
+for (let boton of botones) {
+  agregarEventoBoton(boton.id, boton.detalle);
+}
